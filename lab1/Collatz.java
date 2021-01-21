248633193343b9067fcc7072dbea45d1f691a15c
Bill Hu <hubi@berkeley.edu>
2021-01-20 16:01:54 -0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,11 +1,28 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Bill Hu
  */
 public class Collatz {
+
+    /**
+     * returns the next number in a Collatz sequence
+     * @param n
+     * @return
+     */
+    public static int nextNumber(int n){
+        if (n%2==0){
+            return n/2;
+        }else{
+            return 3*n+1;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
-        System.out.print(n + " ");
-
+        while(n!=1) {
+            System.out.print(n + " ");
+            n=nextNumber(n);
+        }
+        System.out.print(1);
     }
 }
 
