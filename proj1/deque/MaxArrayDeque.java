6085e501b5c6c2141c1357e80761ebf9ca647a70
Bill Hu <hubi@berkeley.edu>
2021-02-02 23:48:39 -0800

    try fixing MAD

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,12 +1,12 @@
 package deque;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
 import java.util.Comparator;
 
 public class MaxArrayDeque<T> extends ArrayDeque {
 
+    /**
+     * Default Comparator for Deque.
+     */
     private Comparator<T> comp;
 
     public MaxArrayDeque(Comparator<T> c) {
@@ -14,35 +14,57 @@ public class MaxArrayDeque<T> extends ArrayDeque {
         comp = c;
     }
 
-    private List<T> getDeque() {
+    /**
+     * Gets the Deque from the parent.
+     *
+     * @return returns the Deque.
+     */
+    private T[] getDeque() {
         int size = super.size();
-        List<T> deque = new ArrayList<T>();
+        T[] deque = (T[]) new Object[size];
         for (int i = 0; i < size; i++) {
-            deque.add((T) super.get(i));
+            deque[i] = (T) super.get(i);
         }
         return deque;
     }
 
+    /**
+     * Returns the max dictated by the default Comparator.
+     *
+     * @return
+     */
     public T max() {
         if (super.isEmpty()) {
             return null;
         }
-        List<T> deque = getDeque();
-        return Collections.max(deque, comp);
+        T[] deque = getDeque();
+        int maxDex = 0;
+        for (int i = 1; i < super.size(); i++) {
+            if (comp.compare(deque[i], deque[maxDex]) > 0) {
+                maxDex = i;
+            }
+        }
+        return deque[maxDex];
     }
 
+    /**
+     * Returns the max dictated by the specified Comparator.
+     *
+     * @param c Comparator to use.
+     * @return
+     */
     public T max(Comparator<T> c) {
         if (super.isEmpty()) {
             return null;
         }
-        List<T> deque = getDeque();
-        return Collections.max(deque, c);
-    }
-
-    public static void main(String[]args){
-//
-//        MaxArrayDeque<Integer> MAD = new MaxArrayDeque<>();
-
+        T[] deque = getDeque();
+        int maxDex = 0;
+        for (int i = 1; i < super.size(); i++) {
+            if (c.compare(deque[i], deque[maxDex]) > 0) {
+                maxDex = i;
+            }
+        }
+        return deque[maxDex];
     }
 
 }
